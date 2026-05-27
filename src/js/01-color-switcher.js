b919611812ff9e69a41935ab194f79b6a493320f
Galina-Goncharova <galinagoncharova1112@gmail.com>
2026-05-27 15:01:04 +0200

    feat: complete color switcher with active buttons and color logging

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,33 @@
 import '../css/common.css';
 
-const color = rgba(0, 0, 0, 0);
+const refStart = document.querySelector('[data-start]');
+const refStop = document.querySelector('[data-stop]');
+const refBody = document.querySelector('body');
+
+let timerId = null;
+
+refStart.addEventListener('click', onChangeColor);
+
+refStop.addEventListener('click', onGetColor);
+
+function onChangeColor() {
+  refStart.disabled = true;
+  refStop.disabled = false;
+  timerId = setInterval(() => {
+    refBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onGetColor() {
+  refStart.disabled = false;
+  refStop.disabled = true;
+
+  console.log('Ваш цвет:', refBody.style.backgroundColor);
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
